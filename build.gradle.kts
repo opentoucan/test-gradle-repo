bbb99b7a0a8462e033056be61525a9b820563b24
sillock <mail@sillock.com>
2025-10-05 14:29:53 +0100

    let him cook

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 
 kotlin {
-    # renovate: datasource=docker depName=jdk
+    # renovate: datasource=java-version depName=jdk
     jvmToolchain(17)
     compilerOptions {
         freeCompilerArgs.addAll("-Xjsr305=strict")
